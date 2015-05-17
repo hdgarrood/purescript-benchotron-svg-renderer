62e52de17c0050de11852af7dadc2c8f4d153d3c
Harry Garrood <harry@garrood.me>
2015-05-17 02:47:09 +0100

    Initial commit

---
+++ benchotronRenderer.js
@@ -0,0 +1,151 @@
+window.BenchotronRenderer = {}
+;(function() {
+
+var margin = {top: 80, right: 20, bottom: 60, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.size); })
+    .y(function(d) { return y(d.stats.mean); });
+
+function takeWhere(arr, pred) {
+  var result = arr.filter(pred);
+  if (result.length === 1) {
+    return result[0];
+  } else {
+    throw new Error('takeWhere: did not return exactly 1 result');
+  }
+}
+
+function renderSeries(svg, seriesIndex, data) {
+  var series = svg.append("g")
+                .attr("class", "data-series-" + String(seriesIndex))
+
+  series.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) { return x(d.size) })
+      .attr("cy", function(d) { return y(d.stats.mean) })
+      .attr("r", 3);
+
+  series.selectAll("path.error-bar")
+      .data(data)
+      .enter()
+        .append("path")
+        .attr("class", "error-bar")
+        .attr("x", function(d) { return x(d.size) })
+        .attr("y", function(d) { return y(d.stats.mean) })
+        .attr("d", function(d) {
+              var mx = x(d.size),
+                  my = y(d.stats.mean),
+                  my1 = y(d.stats.mean + d.stats.deviation),
+                  my2 = y(d.stats.mean - d.stats.deviation),
+                  dx = 3
+              return ["M", mx, my,
+                      "V", my1, "h", -dx, "h", 2 * dx, "h", -dx,
+                      "V", my2, "h", -dx, "h", 2 * dx
+                     ].join(' ')
+            })
+
+  series.append("path")
+       .datum(data)
+       .attr("class", "line")
+       .attr("d", line);
+}
+
+function concatArray(x, y) {
+  return x.concat(y)
+}
+
+function drawGraph(data, elId) {
+  var svg = d3.select(elId).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var allData = data.series
+                    .map(function(x) { return x.results })
+                    .reduce(concatArray, []) // flatten
+
+  x.domain(d3.extent(allData, function(d) { return d.size }))
+  y.domain(d3.extent(allData, function(d) { return d.stats.mean }))
+
+  // Draw title
+  svg.append("g")
+      .attr("transform", "translate(" + (width / 2) + ", 0)")
+     .append("text")
+      .style("text-anchor", "middle")
+      .style("font-size", "200%")
+      .text(data.title)
+
+  // Draw x axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+     .append("text")
+       .attr("x", width / 2)
+       .attr("y", 40)
+       .style("text-anchor", "middle")
+       .text(data.sizeInterpretation);
+
+  // Draw y axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Mean running time (seconds)");
+
+  // Plot each data series
+  data.series.map(function(d, index) {
+    renderSeries(svg, index, d.results);
+  })
+
+  // Draw legend
+  var legend = svg.append("g")
+                  .attr("class", "legend")
+                  .attr("transform", "translate(50, 50)")
+
+  var legendData = data.series.map(function(d) { return d.name })
+  legend.selectAll("rect")
+    .data(legendData)
+    .enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", function(_, i) { return i * 20 })
+      .attr("width", 15)
+      .attr("height", 15)
+      .attr("class", function(_, i) { return "data-series-" + String(i) })
+
+  legend.selectAll("text")
+    .data(legendData)
+    .enter()
+      .append("text")
+      .attr("x", 24)
+      .attr("y", function(_, i) { return i * 20 + 13 })
+      .text(function(d) { return d })
+}
+
+window.BenchotronRenderer.drawGraph = drawGraph
+})()
